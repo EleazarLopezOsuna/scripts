75765c5448247539d588d1481df1ce4d92a0322c
Eleazar Lopez <48371383+EleazarLopezOsuna@users.noreply.github.com>
2022-07-11 18:06:26 -0600

    Update docker_dockerCompose.sh

--- ubuntu/docker_dockerCompose.sh
+++ ubuntu/docker_dockerCompose.sh
@@ -25,3 +25,6 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
